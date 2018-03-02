9de271a968cb4094532ff2a5c279aa27ee0068c4
Prateek Malhotra <prateek@referabuyer.com>
2018-03-02 01:51:18 -0500

    Added token generation test. More travis changes, hopefully the last?

--- integration_test.go
+++ integration_test.go
@@ -20,13 +20,17 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/ory/hydra/config"
 	"github.com/ory/hydra/health"
+	sdk "github.com/ory/hydra/sdk/go/hydra"
 	"github.com/rs/cors"
 	"github.com/someone1/gcp-jwt-go"
 	"golang.org/x/crypto/bcrypt"
+	goauth2 "golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
 
 func generateGCPHydraHandler(t *testing.T) http.Handler {
@@ -41,7 +45,18 @@ func generateGCPHydraHandler(t *testing.T) http.Handler {
 		BCryptWorkFactor:    bcrypt.DefaultCost,
 		AccessTokenLifespan: "5m",
 	}
-	ctx := gcp_jwt.NewContextJWT(context.Background(), &gcp_jwt.IAMSignJWTConfig{ServiceAccount: "test@serviceaccount.com"})
+
+	credsFile, err := ioutil.ReadFile(os.Getenv("GOOGLE_APPLICATION_CREDENTIALS"))
+	if err != nil {
+		t.Fatalf("could not read credentialsFile: %v", err)
+	}
+
+	jwtConfig, err := google.JWTConfigFromJSON(credsFile)
+	if err != nil {
+		t.Fatalf("could not get jwt config: %v", err)
+	}
+
+	ctx := gcp_jwt.NewContextJWT(context.Background(), &gcp_jwt.IAMSignJWTConfig{ServiceAccount: jwtConfig.Email})
 
 	return GenerateHydraHandler(ctx, c, false, cors.Options{})
 }
@@ -51,7 +66,25 @@ func TestIntegration(t *testing.T) {
 	ts := httptest.NewTLSServer(hydra)
 	defer ts.Close()
 	client := ts.Client()
+	ctx := context.WithValue(context.Background(), goauth2.HTTPClient, client)
 
+	forcedCreds := os.Getenv("FORCE_ROOT_CLIENT_CREDENTIALS")
+	credsParts := strings.Split(forcedCreds, ":")
+	if len(credsParts) != 2 {
+		t.Fatalf("FORCE_ROOT_CLIENT_CREDENTIALS not found or incorrect")
+	}
+
+	// Let's get Hydra SDK
+	hydraConfig := &sdk.Configuration{
+		EndpointURL:  ts.URL,
+		ClientID:     credsParts[0],
+		ClientSecret: credsParts[1],
+	}
+	hydraClient, err := sdk.NewSDK(hydraConfig)
+	if err != nil {
+		t.Fatalf("could not get hydra sdk client: %v", err)
+	}
+	oauth2Config := hydraClient.GetOAuth2ClientConfig()
 	// TODO: Come up with some tests...
 
 	t.Run("Health", func(t *testing.T) {
@@ -70,6 +103,9 @@ func TestIntegration(t *testing.T) {
 	})
 
 	t.Run("Oauth2", func(t *testing.T) {
-
+		_, err := oauth2Config.Token(ctx)
+		if err != nil {
+			t.Fatalf("could not get token: %v", err)
+		}
 	})
 }
