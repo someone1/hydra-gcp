fa1e7f678971b65cb8a8bd73e581cac2eb8cb9de
Prateek Malhotra <someone1@gmail.com>
2018-09-21 13:27:18 -0400

    jwk: context updates from hydra implemented

--- jwk/manager_datastore.go
+++ jwk/manager_datastore.go
@@ -19,26 +19,26 @@ type DatastoreManager struct {
 	namespace string
 }
 
-func (d *DatastoreManager) AddKey(set string, key *jose.JSONWebKey) error {
+func (d *DatastoreManager) AddKey(ctx context.Context, set string, key *jose.JSONWebKey) error {
 	return nil
 }
 
-func (d *DatastoreManager) AddKeySet(set string, keys *jose.JSONWebKeySet) error {
+func (d *DatastoreManager) AddKeySet(ctx context.Context, set string, keys *jose.JSONWebKeySet) error {
 	return nil
 }
 
-func (d *DatastoreManager) GetKey(set, kid string) (*jose.JSONWebKeySet, error) {
+func (d *DatastoreManager) GetKey(ctx context.Context, set, kid string) (*jose.JSONWebKeySet, error) {
 	return nil, nil
 }
 
-func (d *DatastoreManager) GetKeySet(set string) (*jose.JSONWebKeySet, error) {
+func (d *DatastoreManager) GetKeySet(ctx context.Context, set string) (*jose.JSONWebKeySet, error) {
 	return nil, nil
 }
 
-func (d *DatastoreManager) DeleteKey(set, kid string) error {
+func (d *DatastoreManager) DeleteKey(ctx context.Context, set, kid string) error {
 	return nil
 }
 
-func (d *DatastoreManager) DeleteKeySet(set string) error {
+func (d *DatastoreManager) DeleteKeySet(ctx context.Context, set string) error {
 	return nil
 }
